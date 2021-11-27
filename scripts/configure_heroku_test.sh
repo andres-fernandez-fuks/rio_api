80d3420b79bd68ef928c01a958b420d0d80c5bd2
Andres <andyfer91@gmail.com>
2021-11-27 20:20:12 -0300

    cambio configuracion de RACK_ENV en la configuracion de heroku test

--- scripts/configure_heroku_test.sh
+++ scripts/configure_heroku_test.sh
@@ -9,5 +9,5 @@ APP_NAME=rio-api-test
 
 heroku apps:create $APP_NAME
 heroku addons:create heroku-postgresql:hobby-dev --app $APP_NAME
-heroku config:set RACK_ENV=staging --app $APP_NAME
+heroku config:set RACK_ENV=production --app $APP_NAME
 heroku drains:add $SUMOLOGIC_SOURCE --app $APP_NAME

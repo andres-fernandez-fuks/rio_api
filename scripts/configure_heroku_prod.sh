f1d54b73f18a4958c3de3ffc7dc0e0f3fe78168f
Andres <andyfer91@gmail.com>
2021-11-27 21:28:36 -0300

    actualizo configure_heroku de test y prod

--- scripts/configure_heroku_prod.sh
+++ scripts/configure_heroku_prod.sh
@@ -10,4 +10,4 @@ APP_NAME=rio-api-prod
 heroku apps:create $APP_NAME
 heroku addons:create heroku-postgresql:hobby-dev --app $APP_NAME
 heroku config:set RACK_ENV=production --app $APP_NAME
-heroku drains:add $SUMOLOGIC_SOURCE --app $APP_NAME
+heroku drains:add $SUMOLOGIC_SOURCE_PROD --app $APP_NAME

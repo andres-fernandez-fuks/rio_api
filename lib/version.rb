20e789d04b59e9e25c3c45137bd4cb6aa02ec24a
Andres <andyfer91@gmail.com>
2021-11-30 19:13:37 -0300

    US003 finalizada

--- lib/version.rb
+++ lib/version.rb
@@ -1,7 +1,7 @@
 class Version
   MAYOR = 0 # Iteracion
-  MINOR = 1 # Historia de usuario
-  PATCH = 2
+  MINOR = 2 # Historia de usuario
+  PATCH = 0
 
   def self.current
     "#{MAYOR}.#{MINOR}.#{PATCH}"

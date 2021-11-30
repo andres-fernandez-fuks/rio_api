30c034763ecfd047acd5b40fad10216eac372f7f
Manuel <manuelsturla@gmail.com>
2021-11-30 18:00:13 -0300

    Corrijo error en pruebas de repositorios usuario para que comiencen sin usuarios creados

--- spec/app/models/repositorio_usuarios_spec.rb
+++ spec/app/models/repositorio_usuarios_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe Persistence::Repositories::RepositorioUsuarios do
   let(:repositorio) { described_class.new }
+  before(:each) do
+    repositorio.delete_all
+  end
   context 'Consulta de usuario' do
       it 'cuando se consulta por un usuario no registrado se devuelve vacío' do
         expect(repositorio.buscar_por_id_telegram('123')).to be nil

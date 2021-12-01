99fdadd7dbfcdb16a6226a327b62e1cfb01c0936
Manuel <manuelsturla@gmail.com>
2021-12-01 17:32:17 -0300

    Agrego estado a las publicaciones en la BDD

--- app/persistence/repositories/repositorio_publicaciones.rb
+++ app/persistence/repositories/repositorio_publicaciones.rb
@@ -11,15 +11,32 @@ module Persistence
       protected
 
       def load_object(a_record)
-        Object.const_get(self.class.model_class).new(a_record[:precio],
-                                                     RepositorioUsuarios.new.find(a_record[:usuario]))
+        publicacion = Object.const_get(self.class.model_class).new(a_record[:precio],
+                                                                   RepositorioUsuarios.new.find(a_record[:usuario]))
+        configurar_estado(publicacion, a_record[:estado])
+        publicacion
       end
 
       def changeset(publicacion)
         {
           precio: publicacion.precio,
-          usuario: publicacion.usuario.id
+          usuario: publicacion.usuario.id,
+          estado: estado_a_string(publicacion.estado)
         }
+      end
+
+      def configurar_estado(publicacion, estado)
+        publicacion.activar if estado == 'Activo'
+      end
+
+      def estado_a_string(estado)
+        if estado.zero?
+          'Revision'
+        elsif estado == 1
+          'Activo'
+        else
+          'Vendido'
+        end
       end
     end
   end
--- spec/app/persistence/repositories/repositorio_publicaciones_spec.rb
+++ spec/app/persistence/repositories/repositorio_publicaciones_spec.rb
@@ -17,13 +17,28 @@ describe Persistence::Repositories::RepositorioPublicaciones do
       publicacion = repositorio.find(id)
       expect(publicacion.precio).to eq 30303
       expect(publicacion.usuario).to eq usuario
-      end
+      expect(publicacion.estado).to eq ESTADO_REVISION
+    end
+
+    it 'guardar una publicacion activa se guarda con el estado correcto' do
+      publicacion.activar
+      id = repositorio.save(publicacion).id
+      publicacion = repositorio.find(id)
+      expect(publicacion.estado).to eq ESTADO_ACTIVO
+    end
   end
 
   context 'Listar publicaciones activas' do
     it 'Si no existe ninguna, listar publicaciones activas devuelve vacío' do
       repositorio.delete_all
       expect(repositorio.buscar_activas.length).to eq 0
+    end
+
+    xit 'si existe una publicacion activa, al listar activas se devuelve correctamente' do
+      publicacion = Publicacion.new(30, usuario)
+      publicacion.activar
+      repositorio.save(publicacion)
+      expect(repositorio.buscar_activas.length).to eq 1
     end
   end
 end

b99a1180b213e199243a67cb0b15664edbb978d1
Flavio Perez Ondarts <perezflavio94@gmail.com>
2021-12-05 11:22:01 -0300

    Agrego step definition para feature publicacion p2p

---
+++ features/step_definitions/publicacion_p2p_steps.rb
@@ -0,0 +1,9 @@
+Dado('que existe una publicación cotizada') do
+  body = {patente: '100', marca: 'FORD', modelo: 'A', anio: 2000, precio: 50_000, id_telegram: @id_telegram}.to_json
+  response = Faraday.post(crear_publicacion_url, body, header)
+  @id_publicacion = JSON(response.body)['id']
+
+  body = {precio: precio}.to_json
+  response = Faraday.post(informe_de_cotizacion_url(@id_publicacion), body)
+  @id_oferta = JSON(response.body)['id']
+end

e569413fa01aa6db53e4856b55f64eeee5b9b40e
Flavio Perez Ondarts <perezflavio94@gmail.com>
2021-12-11 04:03:16 -0300

    Elimino steps no usados

--- features/step_definitions/publicacion_fiubak_steps.rb
+++ features/step_definitions/publicacion_fiubak_steps.rb
@@ -1,24 +1,3 @@
-Dado('que tengo una oferta a un vendedor por {float}') do |precio|
-  body = {nombre: 'nombre', mail: 'mail@gmail.com', id_telegram: '123'}.to_json
-  Faraday.post(crear_usuario_url, body, header)
-  @datos_publicacion = {patente: 'ASD100', marca: 'FORD', modelo: 'A', anio: 2000, precio: precio, id_telegram: '123'}
-  response = Faraday.post(crear_publicacion_url, @datos_publicacion.to_json, header)
-  id_publicacion = JSON(response.body)['id']
-  response = Faraday.post(informe_de_cotizacion_url(id_publicacion), { pracio: 3030}.to_json)
-  @id_oferta = JSON.parse(response.body)['id_oferta']
-end
-
-Cuando('el vendedor la acepta') do
-  body = {estado: 'aceptada'}.to_json
-  @response = Faraday.patch(aceptar_oferta_url(@id_oferta), body, header)
-end
-
-Entonces('publico el auto con un incremento del {float}% a un precio de {float}') do |porcentaje, precio|
-  @datos_publicacion[:precio] = @datos_publicacion[:precio] * (1.0 + (porcentaje / 100))
-  response = Faraday.post(crear_publicacion_url, @datos_publicacion.to_json, header)
-  expect(JSON.parse(response.body)['precio']).to eq precio
-end
-
 Dado('que existe una oferta de FIUBAK para la publicacion') do
   header = { 'ID_TELEGRAM' => @id_telegram }
   Faraday.post(informe_de_cotizacion_url(@id_publicacion), {}.to_json)

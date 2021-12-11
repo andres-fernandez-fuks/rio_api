d94da27a41137afb04a6b233751e140bab094633
Manuel <manuelsturla@gmail.com>
2021-12-11 14:42:25 -0300

    Agrego prueba para que las publicaciones P2P no se puedan reservar

--- spec/app/comandos/reservar_publicacion_spec.rb
+++ spec/app/comandos/reservar_publicacion_spec.rb
@@ -18,23 +18,21 @@ describe ReservarPublicacion do
   }
   let(:publicacion_p2p) {
     publicacion_p2p = PublicacionP2P.new(200000, usuario, auto)
+    publicacion_p2p.activar
     Persistence::Repositories::RepositorioPublicaciones.new.save(publicacion_p2p)
   }
   let(:publicacion_fiubak) {
     publicacion_fiubak = PublicacionFiubak.new(200000, usuario, auto2)
+    publicacion_fiubak.activar
     Persistence::Repositories::RepositorioPublicaciones.new.save(publicacion_fiubak)
   }
-  before(:each){
-    publicacion_p2p.activar
-    publicacion_fiubak.activar
-  }
-
   it 'si es de fiubak la reserva correctamente' do
     described_class.new.ejecutar(publicacion_fiubak)
     expect(Persistence::Repositories::RepositorioPublicaciones.new.find(publicacion_fiubak.id).estado).to eq EstadoReservado.new
   end
 
-  xit 'si es P2P levanta un error y no guarda nada' do
-
+  it 'si es P2P levanta un error y no guarda nada' do
+    expect{described_class.new.ejecutar(publicacion_p2p)}.to raise_error(PublicacionNoReservableError)
+    expect(Persistence::Repositories::RepositorioPublicaciones.new.find(publicacion_p2p.id).estado).to eq EstadoActivo.new
   end
 end

6b51c0eff99bbfd3dc6c7cf448ecd20f13842a89
Ramiro Montiel <rmontiel@fi.uba.ar>
2021-12-12 19:05:33 -0300

    actualizo script de acceptance test para no enviar mails

--- scripts/acceptance_test.sh
+++ scripts/acceptance_test.sh
@@ -4,4 +4,6 @@ export BASE_URL=$1
 
 echo "Running acceptance test on $BASE_URL"
 mkdir -p reports
+curl --location --request POST "$BASE_URL/deshabilitar_mails"
 bundle exec rake acceptance
+curl --location --request POST "$BASE_URL/habilitar_mails"

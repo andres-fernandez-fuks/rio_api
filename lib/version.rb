1a08730c2cd3de1a493f8b7ce35c18c5a2a0dcaf
Ramiro Montiel <rmontiel@fi.uba.ar>
2021-12-12 12:47:35 -0300

    aumento la version

--- lib/version.rb
+++ lib/version.rb
@@ -1,7 +1,7 @@
 class Version
   MAYOR = 2 # Iteracion
-  MINOR = 1 # Historia de usuario
-  PATCH = 1
+  MINOR = 2 # Historia de usuario
+  PATCH = 0
 
   def self.current
     "#{MAYOR}.#{MINOR}.#{PATCH}"
